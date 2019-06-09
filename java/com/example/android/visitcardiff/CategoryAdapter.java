c1994db561a9ae9e45a03749905e9c675f46aad0
MikeySmash <49338753+MikeySmash@users.noreply.github.com>
2019-06-09 20:30:35 +0100

    Update CategoryAdapter.java

--- java/com/example/android/visitcardiff/CategoryAdapter.java
+++ java/com/example/android/visitcardiff/CategoryAdapter.java
@@ -13,13 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.visitcardiff;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
 /**
  * Provides the appropriate {@link Fragment} for a view pager.
@@ -31,11 +46,8 @@ public class CategoryAdapter extends FragmentPagerAdapter {
         super(fragmentManager);
         this.mContext = context;
     }
-    //    String toSee = getString(R.string.to_see);
-    Resources res = mContext.getResources();
-    private String[] tabTitles = res.getStringArray(R.array.tab_headings_array);
 
-    @Override
+     @Override
     public Fragment getItem(int position) {
         if (position == 0) {
             return new ToSeeFragment();
@@ -47,12 +59,17 @@ public class CategoryAdapter extends FragmentPagerAdapter {
             return new ToDoFragment();
         }
     }
-    @Override
-    public CharSequence getPageTitle(int position) {
-        // Generate title based on item position
+    @Override public CharSequence getPageTitle(int position) {
 
-        return tabTitles[position];
-    }
+        if (position == 0) {
+            return mContext.getString(R.string.to_see);
+        } else if (position == 1) {
+            return mContext.getString(R.string.to_eat);
+        } else if (position == 2) {
+            return mContext.getString(R.string.to_stay);
+        } else {
+            return mContext.getString(R.string.to_do);
+        } }
     @Override
     public int getCount() {
         return 4;
